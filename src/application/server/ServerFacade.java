c90b7448b197e78e333b41c26ffcca0c39172a31
Luciano Maiwald <luciano.maiwald@gmail.com>
2013-11-02 11:58:36 +0100

    added main method

--- src/application/server/ServerFacade.java
+++ src/application/server/ServerFacade.java
@@ -1,17 +1,22 @@
 package application.server;
 
 import application.materials_management.facades.MaterialsManagementFacade;
+import application.order_management.data_access.dtos.OrderDTO;
 import application.order_management.facades.OrderManagementFacade;
 import application.production.facades.ProductionFacade;
 
 public class ServerFacade {
-
 
     MaterialsManagementFacade materialsManagement = new MaterialsManagementFacade();
     OrderManagementFacade orderManagement = new OrderManagementFacade();
     ProductionFacade production = new ProductionFacade();
 
     public ServerFacade() {
+        OrderDTO order = orderManagement.createOrder(2345);
+        System.out.println(order);
+    }
 
+    public static void main(String[] args) {
+        new ServerFacade();
     }
 }
