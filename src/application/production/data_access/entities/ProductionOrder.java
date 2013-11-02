88ac0f49220904a27892e82018e78f90e754e5a0
Luciano Maiwald <luciano.maiwald@gmail.com>
2013-11-02 11:46:53 +0100

    made ProductionOrder a hibernate entity

--- src/application/production/data_access/entities/ProductionOrder.java
+++ src/application/production/data_access/entities/ProductionOrder.java
@@ -1,15 +1,43 @@
 package application.production.data_access.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 import application.order_management.data_access.dtos.OrderDTO;
 
+@Entity
 public class ProductionOrder {
 
-    private OrderDTO orderDTO;
+    private int productionOrderId;
+    private int orderId;
     private int partId;
 
     public ProductionOrder(OrderDTO orderDTO) {
-        this.orderDTO = orderDTO;
+        this.orderId = orderDTO.orderId;
         this.partId = 2349085;
+    }
+
+    @Id
+    public int getProductionOrderId() {
+        return productionOrderId;
+    }
+
+    public void setProductionOrderId(int productionOrderId) {
+        this.productionOrderId = productionOrderId;
+    }
+
+    @Column(nullable = false)
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public void setPartId(int partId) {
+        this.partId = partId;
     }
 
     public int getPartId() {
