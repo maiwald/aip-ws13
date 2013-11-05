bf8d62ea96f638f250f744fc25edf8be8ea282aa
engineduck <felix.jensen@haw-hamburg.de>
2013-11-05 12:24:48 +0100

    clean up

--- src/application/materials_management/facades/MaterialsManagement.java
+++ src/application/materials_management/facades/MaterialsManagement.java
@@ -5,11 +5,9 @@ import application.materials_management.data_access.entities.PartDTO;
 import java.util.List;
 
 /**
- * Created with IntelliJ IDEA.
  * User: felix_000
  * Date: 05.11.13
  * Time: 09:08
- * To change this template use File | Settings | File Templates.
  */
 public interface MaterialsManagement {
     List<PartDTO> getPartsList(int partId);
